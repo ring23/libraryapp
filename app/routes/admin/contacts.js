99a9a075fda6eed93bbc4057b8c064a54c5c6012
ring23 <cmeringolo23@gmail.com>
2016-12-17 18:37:41 -0500

    added edit and delete function to contact admin and libraries

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -4,6 +4,18 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('contact');
+  },
+
+  actions: {
+
+    deleteContact(contact) {
+      let confirmation = confirm("Are you sure you want to delete this contact?");
+
+      if (confirmation) {
+        contact.destroyRecord();
+      }
+    }
+
   }
 
 });
